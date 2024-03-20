43442bc6101c44ad7667b8ff3d2b76e690904bd7
unknown <osamaeelnahas43@gmail.com>
2024-03-20 16:13:58 +0200

    solve day1

--- Day1.1.py
+++ Day1.1.py
@@ -1,1 +1,12 @@
 # problem : https://codeforces.com/group/c3FDl9EUi9/contest/262795/problem/B
+from collections import Counter
+
+s = input()
+x = Counter(s)
+l = []
+for key in x.keys():
+    l.append(key)
+
+l.sort()
+for i in range(len(l)):
+    print(l[i], x.get(l[i]))
